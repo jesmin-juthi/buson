528329065aba67007a5d2ed1ff88eb0472d60e36
jesmin-juthi <thesathil67@gmail.com>
2022-02-03 02:56:12 +0600

    post type dynamic

--- index.php
+++ index.php
@@ -430,19 +430,42 @@
                     </div>
                 </div>
                 <div class="row">
+                    <?php 
                     
+                        $args = array(
+                            'post_type' => 'post',
+                            'posts_per_page' => 3,
+                            'order' => 'DESC'
+                        );
+
+                        $query = new WP_Query($args);
+
+                        while($query->have_posts()): $query->the_post();
+                    
+                    ?>
                     <div class="col-xl-4 col-lg-4 col-md-6">
                         <div class="single-recent-cap mb-30">
                             <div class="recent-img">
-                                <img src="<?php echo get_template_directory_uri()?>/assets/img/recent/rcent_1.png" alt="">
+                                <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title();?>">
                             </div>
                             <div class="recent-cap">
-                                <span>Business planing</span>
-                                <h4><a href="single-blog.html">Amazing Places To Visit In Summer</a></h4>
-                                <p>Nov 30, 2020</p>
+                                <span>
+                                    <!-- showing the category name -->
+                                    <?php 
+                                    
+                                        $cats = get_the_category();
+                                        foreach($cats as $cat){
+                                            echo $cat->name. ", ";
+                                        }     
+                                    ?>
+                                </span>
+                                <h4><a href="<?php the_permalink();?>"><?php the_title();?></a></h4>
+                                <p><?php the_date();?></p>
                             </div>
                         </div>
                     </div>
+
+                    <?php endwhile; wp_reset_postdata();?>
 
                 </div>
             </div>
